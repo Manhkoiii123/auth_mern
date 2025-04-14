8a2daadcce64afeefef3621263d818df4f5d9cae
manhtranduc <manhtranduc0202@gmail.com>
2025-04-14 21:46:08 +0700

    model user, session, verification, register api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import { config } from "./config/app.config";
 import connectDatabase from "./database/database";
 import { errorHandler } from "./middlewares/errorHandler";
+import authRoutes from "./modules/auth/auth.routes";
 const app = express();
 const BASE_PATH = config.BASE_PATH;
 app.use(express.json());
@@ -16,7 +17,9 @@ app.use(
   })
 );
 app.use(cookieParser());
+app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(errorHandler);
+
 app.listen(config.PORT, async () => {
   console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
   await connectDatabase();
---
+++ backend/src/modules/auth/auth.routes.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { authController } from "./auth.module";
+
+const authRoutes = Router();
+authRoutes.post("/register", authController.register);
+export default authRoutes;
